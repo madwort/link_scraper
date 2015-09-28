197d96c21db1b142eac500f7e37d91d2ad1e7422
Tom Ward <tomward@fmail.co.uk>
2015-09-28 20:31:09 +0100

    Fix unescape double-quotes
    fixes #2

--- link-scraper.php
+++ link-scraper.php
@@ -47,8 +47,8 @@ foreach ($urls as $url_key => $url) {
   foreach ($a as $key => $my_a) { 
     if ($my_a->href == "") { continue; }
     $this_link = 
-      "    <dt><b>".$my_a->text."</b></dt><dd style="padding-left: 10px;"><a href=\"".$my_a->href.
-      "\" target=\"_blank\">-".$my_a->href."</a></dd>\n"; 
+      "    <dt><b>".$my_a->text."</b></dt><dd style=\"padding-left: 10px;\">".
+      "<a href=\"".$my_a->href."\" target=\"_blank\">-".$my_a->href."</a></dd>\n"; 
     if (strstr($my_a->href,'youtube') || 
         strstr($my_a->href,'vimeo')) 
     {
