072ebf4e90c8a57b2eb5c7fdd5f10e7ca64e5ead
Tom Ward <tomward@fmail.co.uk>
2015-12-19 17:11:13 +0000

    Revert the formatting change that I made - maybe it's right...?

--- link-scraper.php
+++ link-scraper.php
@@ -49,7 +49,7 @@ foreach ($urls as $url_key => $url) {
     if ($my_a->href == "") { continue; }
     $this_link = 
       "    <dt><b>".$my_a->text."</b></dt><dd style=\"padding-left: 10px;\">".
-      "<a href=\"".$my_a->href."\" target=\"_blank\">".$my_a->href."</a></dd>\n"; 
+      "<a href=\"".$my_a->href."\" target=\"_blank\">-".$my_a->href."</a></dd>\n"; 
 	if (strstr($my_a->href,'youtube') || 
 	        strstr($my_a->href,'youtu') || 
 			strstr($my_a->href,'vimeo')) 
