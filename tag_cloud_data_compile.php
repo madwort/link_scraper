2f4d3b5cad8e2f6df9e89565891e30f76f2561b5
Rodrigo Constanzo <rodrigo.constanzo@gmail.com>
2016-01-05 23:02:30 +0000

    updated $keywords as per #17 in wp-plugins repo

--- tag_cloud_data_compile.php
+++ tag_cloud_data_compile.php
@@ -1,8 +1,5 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-use PHPHtmlParser\Dom;
-
 $urls = [
   'http://www.rodrigoconstanzo.com/thesis-introduction/',
   'http://www.rodrigoconstanzo.com/2015/04/making-decisions-in-time/',
@@ -21,57 +18,44 @@ $urls = [
   'http://www.rodrigoconstanzo.com/grassi-box/'
 ];
 
-$keywords["composition"] = ["composed", "compose", "composer"];
+$keywords["composition"] = ["composed", "compose", "composer", "composing", "composition", "compositions"];
 $keywords["improvisation"] = ["improv", "improvised", "improviser", "improvising"];
-$keywords["performance"] = ["performer", "performed"];
+$keywords["performance"] = ["performer", "performed", "performers"];
 $keywords["meta"] = ["meta", "meta-creative"];
 $keywords["diy"] = ["diy", "DIY", "luthier", "build", "instrument"];
 $keywords["software"] = ["software"];
 $keywords["dfscore"] = ["dfscore", "dfs", "networked score", "network score"];
-$keywords["controller"] = ["controller", "gamepad", "monome"];
-$keywords["gamepad"] = ["controller"];
+$keywords["controller"] = ["controller", "controllers", "gamepad", "gamepads", "monome"];
+$keywords["gamepad"] = ["controller", "controllers", "gamepad"];
 $keywords["mapping"] = ["mapping", "mapped", "map"];
-$keywords["gesture"] = ["gesture"];
+$keywords["gesture"] = ["gesture", "gestures"];
 $keywords["form"] = ["form", "formal"];
 $keywords["memory"] = ["memory", "memories"];
-$keywords["interaction"] = ["interact", "interacted"];
-$keywords["behavior"] = ["behavior", "behaviour", "behaviors"];
-$keywords["game"] = ["game", "etude", "battle", "challenge"];
-$keywords["battle"] = ["battle", "game", "etude", "challenge"];
+$keywords["interaction"] = ["interaction", "interactions", "interact", "interacted"];
+$keywords["behavior"] = ["behavior", "behaviors"];
+$keywords["game"] = ["game", "games", "etude", "etudes", "battle", "battles", "challenge", "challenges"];
+$keywords["battle"] = ["battle", "battles", "game", "etude", "etudes", "challenge", "challenges"];
 $keywords["analysis"] = ["analysis", "analyses", "analyzed", "analysed"];
 $keywords["video"] = ["video", "videos", "film", "filmed"];
-$keywords["drums"] = ["drums", "percussion"];
+$keywords["drums"] = ["drums", "drum", "percussion"];
 $keywords["feedback"] = ["feedback"];
 
 echo "tag,term,text,url\r\n";
 
 foreach ($urls as $url_key => $url) {
-  // old string method
+
+  // $url_key = 2;
   // $url = $urls[$url_key];
-  // $html = file_get_contents($url);
-
-  // new method
-  $dom = new Dom;
-  $dom->loadFromUrl($url);
-  $html_dom = $dom->find('#colLeft');
-  $html = (string)$html_dom;
-  if(mb_detect_encoding($html, 'UTF-8, ISO-8859-1') != 'UTF-8') {
-    $html = str_replace(chr(0xC2),'',$html);
-    $html = iconv('ISO-8859-1', 'UTF-8', $html);
-  }
-  // var_dump((string)$html);
-
+  $html = file_get_contents($url);
   $html_no_scripts = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
   $clean_html = html_entity_decode(strip_tags($html_no_scripts));
 
   foreach ($keywords as $keyword => $keyword_alternatives) {
     foreach ($keyword_alternatives as $keyword_alt_id => $keyword_alternative) {
-      // var_dump("keyword:" . $keyword_alternative);
-      // var_dump("clean html:" . $clean_html);
+      // var_dump($keyword_alternative);
       preg_match_all('/.{0,15}\b' . $keyword_alternative .
                      '\b.{0,15}/u', $clean_html, $matches
                     );
-      // var_dump($matches);
       foreach ($matches[0] as $key => $match) {
         // if (htmlspecialchars($match, ENT_QUOTES, "UTF-8") == "") {
         //   var_dump($match);
